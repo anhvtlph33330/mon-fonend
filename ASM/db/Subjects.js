36148fccd0b063d461ef7ea23a76d824d139b851
anhvtlph33330 <anhvtlph33330@fpt.edu.vn>
2024-04-07 21:05:28 +0700

    a

---
+++ ASM/db/Subjects.js
@@ -0,0 +1,37 @@
+[
+    {
+        "Id": "ADAV",
+        "Name": "Lập trình PHP",
+        "Logo": "php.png"
+    },
+    {
+        "Id": "ADBS",
+        "Name": "Lập trình PHP",
+        "Logo": "php (1).png"
+    },
+    {
+        "Id": "ADTE",
+        "Name": "Kiểm thử và triển khai ứng dụng Android",
+        "Logo": "ADTE.jpg"
+    },
+    {
+        "Id": "ADUI",
+        "Name": "Thiết kế giao diện trên Android",
+        "Logo": "ADUI.jpg"
+    },
+    {
+        "Id": "ASNE",
+        "Name": "Lập trình ASP.NET",
+        "Logo": "php (1).png"
+    },
+    {
+        "Id": "CLCO",
+        "Name": "Điện toán đám mây",
+        "Logo": "CLCO.jpg"
+    },
+    {
+        "Id": "DBAV",
+        "Name": "SQL Server",
+        "Logo": "DBAV.png"
+    }
+]
